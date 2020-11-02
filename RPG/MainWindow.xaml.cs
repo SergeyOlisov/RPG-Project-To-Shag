49d154856b739ac8755947ce6351f70ce5caaafc
Sergey-Prisyzhnykh-IT <joker3839@gmail.com>
2020-11-02 21:20:01 +0300

    Windows

--- RPG/MainWindow.xaml.cs
+++ RPG/MainWindow.xaml.cs
@@ -9,33 +9,75 @@ using Microsoft.Win32;
 using RPG.data.helpers;
 using System.Windows.Documents;
 using System.Collections.Generic;
+using System.Windows.Input;
 
 namespace RPG
 {
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
- 
+        static Hero hero = new Hero();
         public MainWindow()
         {
             InitializeComponent();
         }
-
-
-
-        private void Exit_Click(object sender, RoutedEventArgs e)
+        private void Exit_x_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             Close();
         }
-
-        private void Start_Click(object sender, RoutedEventArgs e)
+        private void RollUp_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var locatin1 = new Location1();
-            locatin1.Show();
+            WindowState = WindowState.Minimized;
+        }
+        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+        private void Polygon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+        private void Polygon_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+        private void NewGames_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var Location1 = new Location1();
+            Location1.Show();
             Close();
+
+        }
+        private void Load_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.json)|*.json|(*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                hero = Editor.HeroDeserializeAsync(openFileDialog.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Ошибка загрузки");
+            }
+
+        }
+        private void Setting_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void Authors_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
